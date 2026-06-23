006cf9be536f7fbe6853800bc0f6f5591cc8f05b
Amish Kumar Jha <amishjha3593@gmail.com>
2026-06-24 00:25:00 +0530

    Updated link of deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+// const BASE_URL = import.meta.env.VITE_API_URL;
+// const api = axios.create({
+//   baseURL: "http://localhost:5000/api",
+// });
+
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use(
